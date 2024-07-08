d9f387ef75a0a2725ff524c8fdd80df803f70eb9
manpham-saritasa <man.pham@saritasa.com>
2024-07-08 14:55:49 +0700

    Lint fix from interface to type and add JSDoc to fix angular eslint JC-597

--- apps/angular/src/environments/environment.prod.ts
+++ apps/angular/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
+/**
+ * Application environment configuration for production.
+ *
+ * Defines the environment configuration for the application,
+ * specifying that the application is in production mode.
+ *
+ * @constant
+ * @property {boolean} production Indicates if the application is in production mode.
+ */
 export const environment = {
 	production: true,
 };
