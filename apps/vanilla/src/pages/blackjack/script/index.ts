17be31a1673a9e8059810c7a4177231b8512a269
manpham-saritasa <man.pham@saritasa.com>
2024-07-10 10:21:12 +0700

    Create Dice Instance and implement update player dice result JC-623

--- apps/vanilla/src/pages/blackjack/script/index.ts
+++ apps/vanilla/src/pages/blackjack/script/index.ts
@@ -6,16 +6,16 @@ class Publisher<T> {
 	private subscribers: Subscriber<T>[] = [];
 
 	public subscribe(subscriber: Subscriber<T>): void {
-		const subIdx = this.getSubscriberIndex(subscriber);
-		if (subIdx === -1) {
+		const subcriberIndex = this.getSubscriberIndex(subscriber);
+		if (subcriberIndex === -1) {
 			this.subscribers.push(subscriber);
 		}
 	}
 
 	public unsubscribe(subscriber: Subscriber<T>): void {
-		const subIdx = this.getSubscriberIndex(subscriber);
-		if (subIdx !== -1) {
-			this.subscribers.splice(subIdx, 1);
+		const subcriberIndex = this.getSubscriberIndex(subscriber);
+		if (subcriberIndex !== -1) {
+			this.subscribers.splice(subcriberIndex, 1);
 		}
 	}
 
@@ -24,11 +24,11 @@ class Publisher<T> {
 	}
 
 	public notify(message: T): void {
-		this.subscribers.forEach(subcriber => subcriber.update(message));
+		this.subscribers.forEach((subcriber) => subcriber.update(message));
 	}
 
 	private getSubscriberIndex(subscriber: Subscriber<T>): number {
-		return this.subscribers.findIndex(sub => sub === subscriber);
+		return this.subscribers.findIndex((sub) => sub === subscriber);
 	}
 }
 
@@ -38,16 +38,23 @@ class PlayerTurnResult {
 
 class DiceGenerator extends Publisher<number> {
 	private sidesCount: number;
+	private static instance: DiceGenerator;
 
 	private constructor(sidesCount: number) {
 		super();
 		this.sidesCount = sidesCount;
 	}
 
-	public roll(): number {
+	public static getInstance() {
+		if (DiceGenerator.instance == null) {
+			DiceGenerator.instance = new DiceGenerator(6);
+		}
+		return DiceGenerator.instance;
+	}
+
+	public roll(): void {
 		const diceResult = Math.floor(Math.random() * this.sidesCount) + 1;
 		this.notify(diceResult);
-
 	}
 }
 
@@ -59,13 +66,42 @@ class TurnGenerator extends Publisher<PlayerTurnResult> implements Subscriber<nu
 		this.notify(turnResult);
 		this.currentPlayerIndex = (this.currentPlayerIndex + 1) % this.subscribersCount();
 	}
+
+	public next() {
+		DiceGenerator.getInstance().roll();
+	}
 }
 
 class Player implements Subscriber<PlayerTurnResult> {
 	private diceResult: number[] = [];
-
 	public result: Publisher<number[]> = new Publisher<number[]>();
-
 	public winStatus: Publisher<boolean> = new Publisher<boolean>();
 
+	public constructor(private playerIndex: number) {}
+
+	private calculateTotalDice = (): number => {
+		return this.diceResult.reduce((result, current) => result + current, 0);
+	};
+
+	update(turnResult: PlayerTurnResult) {
+		if (turnResult.playerIndex === this.playerIndex) this.diceResult.push(turnResult.diceResult);
+		this.result.notify([turnResult.diceResult]);
+		const totalDiceResult = this.calculateTotalDice();
+		if (totalDiceResult >= 21) {
+			this.winStatus.notify(true);
+		}
+	}
+}
+
+class ResultDisplayer implements Subscriber<number[]> {
+	private allDiceResult: number[] = [];
+
+	private log() {
+		console.log(this.allDiceResult);
+	}
+
+	update(diceResult: number[]): void {
+		this.allDiceResult.push(...diceResult);
+		this.log();
+	}
 }
