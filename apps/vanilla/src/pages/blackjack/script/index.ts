6e8a53da7bafa211840fd9166db9ae330c7beaad
manpham-saritasa <man.pham@saritasa.com>
2024-07-10 16:02:10 +0700

    Implement the new migration JC-623

--- apps/vanilla/src/pages/blackjack/script/index.ts
+++ apps/vanilla/src/pages/blackjack/script/index.ts
@@ -5,17 +5,21 @@ import { TurnGenerator } from './TurnGenerator';
 
 const player1ResultsElement = document.getElementById('player1-dice')!;
 const player2ResultsElement = document.getElementById('player2-dice')!;
-const allResultsElement = document.getElementById('dice-cap')!;
+const player1TotalScoreElement = document.getElementById('player1-total')!;
+const player2TotalScoreElement = document.getElementById('player2-total')!;
 const rollButton = document.getElementById('button-roll')!;
 
-const player1 = new Player(0, player1ResultsElement);
-const player2 = new Player(1, player2ResultsElement);
+const player1 = new Player(0);
+const player2 = new Player(1);
+
+const player1Displayer = new ResultDisplayer(player1ResultsElement, player1TotalScoreElement);
+const player2Displayer = new ResultDisplayer(player2ResultsElement, player2TotalScoreElement);
+
 const turnGenerator = new TurnGenerator();
 const diceGenerator = DiceGenerator.getInstance();
 
-const allResultsDisplayer = new ResultDisplayer(allResultsElement);
-
-player1.result.subscribe(allResultsDisplayer);
+player1.result.subscribe(player1Displayer);
+player2.result.subscribe(player2Displayer);
 
 turnGenerator.subscribe(player1);
 turnGenerator.subscribe(player2);
