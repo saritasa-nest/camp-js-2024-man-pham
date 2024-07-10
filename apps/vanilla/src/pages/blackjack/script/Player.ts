40c39de1c10ee3a62934bd1695d0567309ec6514
manpham-saritasa <man.pham@saritasa.com>
2024-07-10 13:58:21 +0700

    Implement to the HTML elements JC-623

--- apps/vanilla/src/pages/blackjack/script/Player.ts
+++ apps/vanilla/src/pages/blackjack/script/Player.ts
@@ -1,23 +1,30 @@
 import { Subscriber, Publisher } from './pattern';
-import { PlayerTurnResult } from './ResultDisplayer';
+import { PlayerTurnResult } from './TurnGenerator';
 
 export class Player implements Subscriber<PlayerTurnResult> {
 	private diceResult: number[] = [];
 	public result: Publisher<number[]> = new Publisher<number[]>();
 	public winStatus: Publisher<boolean> = new Publisher<boolean>();
 
-	public constructor(private playerIndex: number) {}
+	public constructor(private playerIndex: number, private element: HTMLElement | null) {}
 
 	private calculateTotalDice = (): number => {
 		return this.diceResult.reduce((result, current) => result + current, 0);
 	};
 
 	update(turnResult: PlayerTurnResult) {
-		if (turnResult.playerIndex === this.playerIndex) this.diceResult.push(turnResult.diceResult);
+		if (turnResult.playerIndex === this.playerIndex) {
+			this.diceResult.push(turnResult.diceResult);
+			this.render();
+		}
 		this.result.notify([turnResult.diceResult]);
 		const totalDiceResult = this.calculateTotalDice();
 		if (totalDiceResult >= 21) {
 			this.winStatus.notify(true);
 		}
+	}
+
+	private render() {
+		if (this.element) this.element.innerText = `${this.diceResult.join(', ')}`;
 	}
 }
--- apps/vanilla/src/pages/blackjack/script/ResultDisplayer.ts
+++ apps/vanilla/src/pages/blackjack/script/ResultDisplayer.ts
@@ -3,12 +3,14 @@ import { Subscriber } from './pattern';
 export class ResultDisplayer implements Subscriber<number[]> {
 	private allDiceResult: number[] = [];
 
-	private log() {
-		console.log(this.allDiceResult);
+	public constructor(private element: HTMLElement | null) {}
+
+	private render() {
+		if (this.element) this.element.innerText = `${this.allDiceResult.join(', ')}`;
 	}
 
 	update(diceResult: number[]): void {
 		this.allDiceResult.push(...diceResult);
-		this.log();
+		this.render();
 	}
 }
