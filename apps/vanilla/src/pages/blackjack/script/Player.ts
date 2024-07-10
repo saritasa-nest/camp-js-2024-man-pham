9cbe713977296a4e531e97b0fabb7f4297eced33
manpham-saritasa <man.pham@saritasa.com>
2024-07-10 16:25:44 +0700

    Add totalScore property JC-623

--- apps/vanilla/src/pages/blackjack/script/Player.ts
+++ apps/vanilla/src/pages/blackjack/script/Player.ts
@@ -3,25 +3,24 @@ import { PlayerTurnResult } from './TurnGenerator';
 
 export class Player implements Subscriber<PlayerTurnResult> {
 	private diceResult: number[] = [];
+	private totalScore: number = 0;
 
 	public result: Publisher<number[]> = new Publisher<number[]>();
 	public winStatus: Publisher<boolean> = new Publisher<boolean>();
 
 	public constructor(private playerIndex: number) {}
 
-	private calculateTotalDice = (): number => {
-		return this.diceResult.reduce((result, current) => result + current, 0);
+	private calculateTotalDice = (): void => {
+		this.totalScore = this.diceResult.reduce((result, current) => result + current, 0);
 	};
 
 	update(turnResult: PlayerTurnResult) {
 		if (turnResult.playerIndex === this.playerIndex) {
 			this.diceResult.push(turnResult.diceResult);
+			this.calculateTotalDice();
 		}
-
 		this.result.notify(this.diceResult);
-
-		const totalDiceResult = this.calculateTotalDice();
-		if (totalDiceResult >= 21) {
+		if (this.totalScore >= 21) {
 			this.winStatus.notify(true);
 		}
 	}
