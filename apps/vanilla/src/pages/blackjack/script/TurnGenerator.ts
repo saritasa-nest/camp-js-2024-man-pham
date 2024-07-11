39968615847de11b0548df9d9161aa15e3c0070b
manpham-saritasa <man.pham@saritasa.com>
2024-07-11 14:55:41 +0700

    Change TurnGenerator constructor JC-623

--- apps/vanilla/src/pages/blackjack/script/TurnGenerator.ts
+++ apps/vanilla/src/pages/blackjack/script/TurnGenerator.ts
@@ -7,7 +7,9 @@ export class PlayerTurnResult {
 
 export class TurnGenerator extends Publisher<PlayerTurnResult> implements Subscriber<number> {
 	private currentPlayerIndex = 0;
-	private playerCount = 2;
+	public constructor(private playerCount: number) {
+		super();
+	}
 
 	public update(diceResult: number): void {
 		const turnResult = new PlayerTurnResult(this.currentPlayerIndex, diceResult);
