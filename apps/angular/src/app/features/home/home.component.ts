c96df6b542bf8f5cf719d60a1699fbb98863b9a7
manpham-saritasa <man.pham@saritasa.com>
2024-07-18 23:36:02 +0700

    Convert to Home Component JC-637

---
+++ apps/angular/src/app/features/home/home.component.ts
@@ -0,0 +1,37 @@
+import { Component, inject } from '@angular/core';
+
+import { Observable } from 'rxjs';
+
+import { TAnime } from '@js-camp/core/models/anime';
+
+import { AnimeResponse, AnimeService } from '@js-camp/angular/core/services/anime.service';
+import { CommonModule } from '@angular/common';
+
+import { AnimeTableComponent } from './table/table.component';
+
+/** Example component. */
+@Component({
+	selector: 'camp-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.css'],
+	imports: [AnimeTableComponent, CommonModule],
+	standalone: true,
+})
+export class HomeComponent {
+	private readonly animeResponse$: Observable<AnimeResponse>;
+
+	private readonly animeService: AnimeService = inject(AnimeService);
+
+	/** Anime list. */
+	protected animeList: TAnime[] = [];
+
+	public constructor() {
+		this.animeResponse$ = this.animeService.getAllAnime();
+
+		this.animeResponse$.subscribe(
+			response => {
+				this.animeList = [...response.items];
+			},
+		);
+	}
+}
