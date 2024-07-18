c8c3b5fc9ae8265a6908e16da1bca62d368daade
manpham-saritasa <man.pham@saritasa.com>
2024-07-18 21:39:03 +0700

    Implemenet the app url service JC-637

--- apps/angular/src/core/services/anime.service.ts
+++ apps/angular/src/core/services/anime.service.ts
@@ -4,14 +4,13 @@ import { HttpClient } from '@angular/common/http';
 
 import { map, Observable } from 'rxjs';
 
-import { environment } from '@js-camp/angular/environments/environment';
-
 import { AnimeMapper } from '@js-camp/core/mappers/anime.mapper';
 import { TAnime } from '@js-camp/core/models/anime';
 import { PaginationMapper } from '@js-camp/core/mappers/pagination.mapper';
 import { PaginationDto } from '@js-camp/core/dtos/pagination.dto';
 import { Pagination } from '@js-camp/core/models/pagination';
 import { TAnimeDto } from '@js-camp/core/dtos/anime.dto';
+import { AppUrlsConfig } from '@js-camp/angular/app/shared/app-url';
 
 /** Anime pagination Dto. */
 type AnimeResponseDto = PaginationDto<TAnimeDto>;
@@ -25,7 +24,7 @@ export type AnimeResponse = Pagination<TAnime>;
 })
 
 export class AnimeService {
-	private readonly baseApiUrl = new URL('anime/', environment.api);
+	private readonly appUrlsConfig = inject(AppUrlsConfig);
 
 	private readonly httpClient = inject(HttpClient);
 
@@ -34,8 +33,7 @@ export class AnimeService {
 	private readonly paginationMapper = inject(PaginationMapper);
 
 	public getAllAnime(): Observable<AnimeResponse> {
-		const url = new URL('anime/', this.baseApiUrl);
-		return this.httpClient.get<AnimeResponseDto>(url.toString()).pipe(
+		return this.httpClient.get<AnimeResponseDto>(this.appUrlsConfig.anime.list).pipe(
 			map(responseDto => this.paginationMapper.mapPaginationFromDto(responseDto, this.animeMapper)),
 		);
 	}
