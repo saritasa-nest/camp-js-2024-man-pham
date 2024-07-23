4f1a1fd6eb30b5d12a785ce75f648fb72e5c8eb4
manpham-saritasa <man.pham@saritasa.com>
2024-07-23 10:51:22 +0700

    Rename table into anime-table and migrate all the service handling into it JC-637

--- apps/angular/src/app/features/home/anime-table/anime-table.component.ts
+++ apps/angular/src/app/features/home/anime-table/anime-table.component.ts
@@ -1,24 +1,29 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
 import { Observable } from 'rxjs';
-import { AnimeResponse } from '@js-camp/angular/core/services/anime.service';
+import { AnimeResponse, AnimeService } from '@js-camp/angular/core/services/anime.service';
 import { NoEmptyStringPipe } from '@js-camp/angular/core/pipes/no-empty-string.pipe';
 
 /** Anime Table Component. */
 @Component({
-	selector: 'camp-table',
+	selector: 'camp-anime-table',
 	standalone: true,
 	imports: [CommonModule, MatTableModule, NoEmptyStringPipe],
-	templateUrl: './table.component.html',
-	styleUrl: './table.component.css',
+	templateUrl: './anime-table.component.html',
+	styleUrl: './anime-table.component.css',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AnimeTableComponent {
-	/** Anime data list. */
-	@Input() public animeResponse$!: Observable<AnimeResponse>;
+	/** Anime response observable. */
+	protected readonly animeResponse$: Observable<AnimeResponse>;
+
+	private readonly animeService: AnimeService = inject(AnimeService);
+
+	public constructor() {
+		this.animeResponse$ = this.animeService.getAllAnime();
+	}
 
 	/** Displayed columns. */
 	protected readonly displayedColumns: string[] = ['image', 'title_eng', 'title_jpn', 'aired.start', 'type', 'status'];
-
 }
--- apps/angular/src/app/features/home/home.component.ts
+++ apps/angular/src/app/features/home/home.component.ts
@@ -1,28 +1,13 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 
-import { Observable } from 'rxjs';
-
-import { AnimeResponse, AnimeService } from '@js-camp/angular/core/services/anime.service';
-import { CommonModule } from '@angular/common';
-
-import { AnimeTableComponent } from './table/table.component';
+import { AnimeTableComponent } from './anime-table/anime-table.component';
 
 /** Example component. */
 @Component({
 	selector: 'camp-home',
 	templateUrl: './home.component.html',
 	styleUrls: ['./home.component.css'],
-	imports: [AnimeTableComponent, CommonModule],
+	imports: [AnimeTableComponent],
 	standalone: true,
 })
-export class HomeComponent {
-	/** Anime response observable. */
-	protected readonly animeResponse$: Observable<AnimeResponse>;
-
-	private readonly animeService: AnimeService = inject(AnimeService);
-
-	public constructor() {
-		this.animeResponse$ = this.animeService.getAllAnime();
-	}
-
-}
+export class HomeComponent {}
