12883f0dc475b1b28f475d91823d05c17900b019
manpham-saritasa <man.pham@saritasa.com>
2024-07-29 15:38:59 +0700

    Develop paginator JC-655

---
+++ apps/angular/src/app/features/home/anime-catalog/components/anime-paginator/anime-paginator.component.ts
@@ -0,0 +1,36 @@
+
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+
+/** Anime Paginator Component. */
+@Component({
+	selector: 'camp-anime-paginator',
+	standalone: true,
+	imports: [CommonModule, MatPaginatorModule],
+	templateUrl: './anime-paginator.component.html',
+	styleUrl: './anime-paginator.component.css',
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AnimePaginatorComponent {
+	/** Page size options. */
+	protected pageSizeOptions: readonly number[] = [5, 10, 20];
+
+	/** Amount of items per page. */
+	protected readonly pageSize: number = 10;
+
+	/** Total amount of fetched items. */
+	@Input() public totalCount = 0;
+
+	/** Event emitter for page changing. */
+	@Output() public pageChange = new EventEmitter<PageEvent>();
+
+	/**
+	 * Emit the page event.
+	 * @param event The page event.
+	 */
+	public onPageChange(event: PageEvent): void {
+		this.pageChange.emit(event);
+	}
+
+}
