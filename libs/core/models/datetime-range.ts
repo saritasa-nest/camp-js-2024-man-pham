5ba1c3378d8ac41cd251c07b793d37e879111ccf
manpham-saritasa <man.pham@saritasa.com>
2024-07-30 22:50:12 +0700

    Add readonly JC-637

--- libs/core/models/datetime-range.ts
+++ libs/core/models/datetime-range.ts
@@ -1,9 +1,9 @@
 /** Date Time Range Type. */
-export type DateTimeRange = {
+export type DateTimeRange = Readonly<{
 
 	/** Start date. */
 	startDate: Date | null;
 
 	/** End date. */
 	endDate: Date | null;
-};
+}>;
