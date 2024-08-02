373a137cbba673858756777d7df519c056382d89
manpham-saritasa <man.pham@saritasa.com>
2024-08-02 10:08:35 +0700

    Create anime columns constant JC-655

---
+++ libs/core/contants/anime-columns.ts
@@ -0,0 +1,9 @@
+/** Anime column ids. */
+export enum AnimeColumns {
+	Image = 'image',
+	TitleEng = 'titleEng',
+	TileJpn = 'titleJpn',
+	StartDate = 'startDate',
+	Type = 'type',
+	Status = 'status',
+}
