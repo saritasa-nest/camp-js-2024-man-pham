406b76f2c7ec7ee0d40cfe4e304db3a2edfddb54
manpham-saritasa <man.pham@saritasa.com>
2024-08-06 18:21:53 +0700

    Remove unused functions JC-655

--- apps/angular/src/core/services/query-params.service.ts
+++ apps/angular/src/core/services/query-params.service.ts
@@ -6,31 +6,14 @@ import { ActivatedRoute, Router } from '@angular/router';
 	providedIn: 'root',
 })
 export class QueryParamsService {
-	public constructor(
-		private readonly router: Router,
-		private readonly activatedRoute: ActivatedRoute,
-	) {}
+	public constructor(private readonly router: Router, private readonly activatedRoute: ActivatedRoute) {}
 
 	/**
 	 * Remove undefined fields.
 	 * @param obj Object to remove.
 	 */
 	private removeUndefinedFields<T extends Record<string, unknown>>(obj: T): Partial<T> {
-		return Object.fromEntries(
-			Object.entries(obj).filter(([_, value]) => value !== undefined),
-		) as Partial<T>;
-	}
-
-	/**
-	 * Obtain value from provided key.
-	 * @param key Key of the query param.
-	 */
-	public obtain(key: string): string | null {
-		const value = this.activatedRoute.snapshot.queryParamMap.get(key);
-		if (value == null) {
-			return null;
-		}
-		return value;
+		return Object.fromEntries(Object.entries(obj).filter(([_, value]) => value !== undefined)) as Partial<T>;
 	}
 
 	/**
@@ -43,35 +26,6 @@ export class QueryParamsService {
 			queryParams: paramsWithoutUndefinedField,
 			relativeTo: this.activatedRoute,
 			queryParamsHandling: 'merge',
-		});
-	}
-
-	/**
-	 * Remove specific param from the query.
-	 * @param param Param to remove.
-	 */
-	public remove(param: string): void {
-		const hasParam = this.activatedRoute
-			.snapshot
-			.queryParamMap
-			.get(param) != null;
-
-		if (hasParam) {
-			this.router.navigate([], {
-				queryParams: {
-					[param]: null,
-				},
-				relativeTo: this.activatedRoute,
-				queryParamsHandling: 'merge',
-			});
-		}
-	}
-
-	/** Reset all query params. */
-	public reset(): void {
-		this.router.navigate([], {
-			queryParams: null,
-			relativeTo: this.activatedRoute,
 		});
 	}
 
