bed05184773c7bd148cd3113b005169a332c79c6
manpham-saritasa <man.pham@saritasa.com>
2024-08-09 12:05:52 +0700

    Change routes configuration JC-674

--- apps/angular/src/app/features/auth/auth.routes.ts
+++ apps/angular/src/app/features/auth/auth.routes.ts
@@ -2,15 +2,21 @@ import { Routes } from '@angular/router';
 
 /** Routes object. */
 export const authRoutes: Routes = [
-	{ path: '', redirectTo: 'login', pathMatch: 'full' },
 	{
-		path: 'login',
-		title: 'Login',
-		loadComponent: () => import('./login/login.component').then(c => c.LoginComponent),
-	},
-	{
-		path: 'register',
-		title: 'Register',
-		loadComponent: () => import('./register/register.component').then(c => c.RegisterComponent),
+		path: '',
+		loadComponent: () => import('./auth.component').then(c => c.AuthComponent),
+		children: [
+			{ path: '', redirectTo: 'login', pathMatch: 'full' },
+			{
+				path: 'login',
+				title: 'Login',
+				loadComponent: () => import('./login/login.component').then(c => c.LoginComponent),
+			},
+			{
+				path: 'register',
+				title: 'Register',
+				loadComponent: () => import('./register/register.component').then(c => c.RegisterComponent),
+			},
+		],
 	},
 ];
