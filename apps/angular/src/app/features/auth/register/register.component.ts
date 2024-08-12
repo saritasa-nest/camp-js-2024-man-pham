1044fe390ae037f2c64a11277f41d0c4a7024f43
manpham-saritasa <man.pham@saritasa.com>
2024-08-12 16:03:23 +0700

    Initialize register form JC-674

--- apps/angular/src/app/features/auth/register/register.component.ts
+++ apps/angular/src/app/features/auth/register/register.component.ts
@@ -1,13 +1,17 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { NonNullableFormBuilder } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { RegisterForm } from '@js-camp/angular/core/models/register-form';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 /** Register component. */
 @Component({
 	selector: 'camp-register',
 	standalone: true,
-	imports: [CommonModule],
+	imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule, RouterLink],
 	templateUrl: './register.component.html',
 	styleUrl: './register.component.css',
 	changeDetection: ChangeDetectionStrategy.OnPush,
@@ -17,4 +21,16 @@ export class RegisterComponent {
 
 	/** Register form group. */
 	protected readonly registerForm = RegisterForm.initialize(this.fb);
+
+	/** Hide password flag. */
+	protected readonly hidePassword = signal(true);
+
+	/**
+	 * Handles hide password button click.
+	 * @param event The click event.
+	 *  */
+	protected clickHidePassword(event: MouseEvent): void {
+		this.hidePassword.set(!this.hidePassword());
+		event.stopPropagation();
+	}
 }
