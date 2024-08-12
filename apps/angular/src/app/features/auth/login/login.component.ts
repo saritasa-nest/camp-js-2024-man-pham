fda180d7bee9dc7adfced32c7d48823f93542d92
manpham-saritasa <man.pham@saritasa.com>
2024-08-12 11:06:14 +0700

    Initilize implementing login services JC-674

--- apps/angular/src/app/features/auth/login/login.component.ts
+++ apps/angular/src/app/features/auth/login/login.component.ts
@@ -1,11 +1,13 @@
 import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
-import { LoginForm } from '@js-camp/angular/core/models/login';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
+import { UserService } from '@js-camp/angular/core/services/user.service';
+import { LoginForm } from '@js-camp/angular/core/models/login-form';
+import { Login } from '@js-camp/core/models/login';
 
 /** Login component. */
 @Component({
@@ -18,6 +20,8 @@ import { RouterLink } from '@angular/router';
 })
 export class LoginComponent {
 	private readonly fb = inject(NonNullableFormBuilder);
+
+	private readonly userService = inject(UserService);
 
 	/** Login form. */
 	protected readonly loginForm = LoginForm.initialize(this.fb);
@@ -32,5 +36,14 @@ export class LoginComponent {
 	protected clickHidePassword(event: MouseEvent): void {
 		this.hidePassword.set(!this.hidePassword());
 		event.stopPropagation();
+	}
+
+	/** Submit handler. */
+	public onSubmit(): void {
+		if (this.loginForm.invalid) {
+			return;
+		}
+		const loginData = new Login(this.loginForm.getRawValue());
+		this.userService.login(loginData).subscribe();
 	}
 }
