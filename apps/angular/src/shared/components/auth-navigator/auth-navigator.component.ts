990d9a46d314b401f309a7bcfa348e91cbb7229f
manpham-saritasa <man.pham@saritasa.com>
2024-08-15 10:22:01 +0700

    Initialize authentication navigator JC-674

---
+++ apps/angular/src/shared/components/auth-navigator/auth-navigator.component.ts
@@ -0,0 +1,28 @@
+import { AsyncPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+import { UserService } from '@js-camp/angular/core/services/user.service';
+import { first } from 'rxjs';
+
+/** Authentication navigator. */
+@Component({
+	selector: 'camp-auth-navigator',
+	standalone: true,
+	imports: [MatButtonModule, AsyncPipe, RouterLink],
+	templateUrl: './auth-navigator.component.html',
+	styleUrl: './auth-navigator.component.css',
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AuthNavigatorComponent {
+	private readonly userService = inject(UserService);
+
+	/** Currently logged in user. */
+	protected readonly currentUser$ = this.userService.currentUser$;
+
+	/** Logs the user out. */
+	protected logOut(): void {
+		this.userService.logout().pipe(first())
+			.subscribe();
+	}
+}
