c357a4168a03cccb92c202f81f0de84c8e3d1fe9
manpham-saritasa <man.pham@saritasa.com>
2024-08-16 14:46:23 +0700

    Handle side effect when input status changed JC-674

--- apps/angular/src/shared/components/input-password/input-password.component.ts
+++ apps/angular/src/shared/components/input-password/input-password.component.ts
@@ -1,16 +1,21 @@
 import {
 	ChangeDetectionStrategy,
+	ChangeDetectorRef,
 	Component,
+	DestroyRef,
 	inject,
 	Input,
 	signal,
+	OnInit,
 } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { FormErrorService } from '@js-camp/angular/core/services/form-error.service';
+import { tap } from 'rxjs';
 
 /** Input with password type. */
 @Component({
@@ -21,21 +26,41 @@ import { FormErrorService } from '@js-camp/angular/core/services/form-error.serv
 	styleUrl: './input-password.component.css',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class InputPasswordComponent {
+export class InputPasswordComponent implements OnInit {
 
 	/** Password form control. */
 	@Input()
 	public control = new FormControl();
 
+	/** Input touched status. */
+	@Input()
+	public touched = false;
+
 	/** Input placeholder. */
 	@Input()
 	public placeholder = 'Enter your password';
+
+	private readonly changeDetector = inject(ChangeDetectorRef);
+
+	private readonly destroyRef = inject(DestroyRef);
 
 	/** Form error. */
 	protected readonly formErrorService = inject(FormErrorService);
 
 	/** Hide password flag. */
 	protected readonly hidePassword = signal(true);
+
+	/** Side effect to ensure checking the input when its status changed. */
+	public ngOnInit(): void {
+		this.control.statusChanges
+			.pipe(
+				tap(() => {
+					this.changeDetector.markForCheck();
+				}),
+				takeUntilDestroyed(this.destroyRef),
+			)
+			.subscribe();
+	}
 
 	/**
 	 * Handles hide password button click.
