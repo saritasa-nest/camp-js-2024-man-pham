d96165175c8c74a4e53c1f8364ce6cb0fec5a70a
manpham-saritasa <man.pham@saritasa.com>
2024-08-19 23:34:28 +0700

    Fix get trailer url and develop get item name list function JC-692

--- apps/angular/src/app/features/home/anime-detail/anime-detail.component.ts
+++ apps/angular/src/app/features/home/anime-detail/anime-detail.component.ts
@@ -1,10 +1,15 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, DatePipe } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
 import { AnimeService } from '@js-camp/angular/core/services/anime.service';
 import { Observable, of } from 'rxjs';
 import { AnimeDetails } from '@js-camp/core/models/anime-detail';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { NoEmptyPipe } from '@js-camp/angular/core/pipes/no-empty.pipe';
+import { AnimeGenre } from '@js-camp/core/models/anime-genre';
+import { AnimeStudio } from '@js-camp/core/models/anime-studio';
 
 const EMBDED_LINK = 'https://www.youtube.com/embed/';
 
@@ -12,7 +17,7 @@ const EMBDED_LINK = 'https://www.youtube.com/embed/';
 @Component({
 	selector: 'camp-anime-detail',
 	standalone: true,
-	imports: [AsyncPipe],
+	imports: [AsyncPipe, MatCardModule, NoEmptyPipe, MatTabsModule, DatePipe],
 	templateUrl: './anime-detail.component.html',
 	styleUrl: './anime-detail.component.css',
 	changeDetection: ChangeDetectionStrategy.OnPush,
@@ -35,6 +40,14 @@ export class AnimeDetailComponent {
 	 * Get anime trailer based on its id.
 	 * @param id Anime id.
 	 */
-	protected getAnimeTrailerUrl = (id: AnimeDetails['id']): SafeResourceUrl =>
+	protected getAnimeTrailerUrl = (id: AnimeDetails['trailerUrl']): SafeResourceUrl =>
 		this.domSanitizer.bypassSecurityTrustResourceUrl(`${EMBDED_LINK}${id}`);
+
+	/**
+	 * Join array of items into a string containing the item's name.
+	 * @param array Array of items.
+	 */
+	protected getItemNameList(array: readonly AnimeGenre[] | readonly AnimeStudio[]): string {
+		return array.map((item, index) => `${item.name}${index === array.length - 1 ? '' : ', '}`).join('');
+	}
 }
