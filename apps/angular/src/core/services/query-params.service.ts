a90f5e28ae1d738daa047f39960c2961dc5ef5f5
manpham-saritasa <man.pham@saritasa.com>
2024-08-28 14:57:29 +0700

    Move to type declaration JC-655

--- apps/angular/src/core/services/query-params.service.ts
+++ apps/angular/src/core/services/query-params.service.ts
@@ -5,7 +5,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 @Injectable({
 	providedIn: 'root',
 })
-export class QueryParamsService {
+export class QueryParamsService<T extends Record<string, unknown>> {
 	private readonly router = inject(Router);
 
 	private readonly activatedRoute = inject(ActivatedRoute);
@@ -16,7 +16,7 @@ export class QueryParamsService {
 	 * The `params` argument uses `any` for flexibility, allowing various types of values.
 	 */
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	public append(params: Record<string, any>): void {
+	public append(params: T): void {
 		const paramsWithoutUndefinedField = this.removeUndefinedFields(params);
 		this.router.navigate([], {
 			queryParams: paramsWithoutUndefinedField,
@@ -29,7 +29,7 @@ export class QueryParamsService {
 	 * Remove undefined fields.
 	 * @param obj Object to remove.
 	 */
-	private removeUndefinedFields<T extends Record<string, unknown>>(obj: T): Partial<T> {
+	private removeUndefinedFields(obj: T): Partial<T> {
 		return Object.fromEntries(Object.entries(obj).filter(([_, value]) => value !== undefined)) as Partial<T>;
 	}
 }
