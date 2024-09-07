f2fa9194e424c121e34990ba8112a282948c0ab6
manpham-saritasa <man.pham@saritasa.com>
2024-09-07 10:29:05 +0700

    Implement new mapper functions JC-692

--- apps/angular/src/core/mappers/anime-detail.mapper.ts
+++ apps/angular/src/core/mappers/anime-detail.mapper.ts
@@ -11,7 +11,7 @@ import { AnimeRatingMapper } from './anime-rating.mapper';
 import { AnimeSeasonMapper } from './anime-season.mapper';
 import { AnimeSourceMapper } from './anime-source.mapper';
 
-/** Anime mapper. */
+/** Anime detail mapper. */
 @Injectable({
 	providedIn: 'root',
 })
@@ -28,29 +28,13 @@ export class AnimeDetailMapper implements TMapper<AnimeDetailDto, AnimeDetail> {
 
 	private readonly studioMapper = inject(AnimeStudioMapper);
 
-	private mapGenreListFromDto(dto: AnimeDetailDto['genres_data']): AnimeDetail['genreList'] {
-		return dto.map(genreDto => this.genreMapper.fromDto(genreDto));
-	}
-
-	private mapGenreListToDto(model: AnimeDetail['genreList']): AnimeDetailDto['genres_data'] {
-		return model.map(genre => this.genreMapper.toDto(genre));
-	}
-
-	private mapStudioListFromDto(dto: AnimeDetailDto['studios_data']): AnimeDetail['studioList'] {
-		return dto.map(studioDto => this.studioMapper.fromDto(studioDto));
-	}
-
-	private mapStudioListToDto(model: AnimeDetail['studioList']): AnimeDetailDto['studios_data'] {
-		return model.map(studio => this.studioMapper.toDto(studio));
-	}
-
 	/** @inheritdoc */
 	public fromDto(dto: AnimeDetailDto): AnimeDetail {
 		return new AnimeDetail({
 			...this.animeMapper.fromDto(dto),
-			rating: this.ratingMapper.MAP_ANIME_RATING_FROM_DTO[dto.rating],
-			season: this.seasonMapper.MAP_ANIME_SEASON_FROM_DTO[dto.season],
-			source: this.sourceMapper.MAP_ANIME_SOURCE_FROM_DTO[dto.source],
+			rating: this.ratingMapper.fromDto(dto.rating),
+			season: this.seasonMapper.fromDto(dto.season),
+			source: this.sourceMapper.fromDto(dto.source),
 			genreList: this.mapGenreListFromDto(dto.genres_data),
 			studioList: this.mapStudioListFromDto(dto.studios_data),
 			trailerUrl: dto.trailer_youtube_id,
@@ -67,9 +51,9 @@ export class AnimeDetailMapper implements TMapper<AnimeDetailDto, AnimeDetail> {
 	public toDto(model: AnimeDetail): AnimeDetailDto {
 		return {
 			...this.animeMapper.toDto(model),
-			rating: this.ratingMapper.MAP_ANIME_RATING_TO_DTO[model.rating],
-			season: this.seasonMapper.MAP_ANIME_SEASON_TO_DTO[model.season],
-			source: this.sourceMapper.MAP_ANIME_SOURCE_TO_DTO[model.source],
+			rating: this.ratingMapper.toDto(model.rating),
+			season: this.seasonMapper.toDto(model.season),
+			source: this.sourceMapper.toDto(model.source),
 			genres_data: this.mapGenreListToDto(model.genreList),
 			studios_data: this.mapStudioListToDto(model.studioList),
 			trailer_youtube_id: model.trailerUrl,
@@ -80,5 +64,21 @@ export class AnimeDetailMapper implements TMapper<AnimeDetailDto, AnimeDetail> {
 			broadcast_time: model.broadcastTime,
 			broadcast_timezone: model.broadcastTimezone,
 		};
+	}
+
+	private mapGenreListFromDto(dto: AnimeDetailDto['genres_data']): AnimeDetail['genreList'] {
+		return dto.map(genreDto => this.genreMapper.fromDto(genreDto));
+	}
+
+	private mapGenreListToDto(model: AnimeDetail['genreList']): AnimeDetailDto['genres_data'] {
+		return model.map(genre => this.genreMapper.toDto(genre));
+	}
+
+	private mapStudioListFromDto(dto: AnimeDetailDto['studios_data']): AnimeDetail['studioList'] {
+		return dto.map(studioDto => this.studioMapper.fromDto(studioDto));
+	}
+
+	private mapStudioListToDto(model: AnimeDetail['studioList']): AnimeDetailDto['studios_data'] {
+		return model.map(studio => this.studioMapper.toDto(studio));
 	}
 }
